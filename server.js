c744fbe9e7c78d730fc6e6d9fb2ec31ebba0b0d5
caffeine-and-chocolate <chuenelesedi8@gmail.com>
2026-06-19 15:10:58 +0200

    fixed port number

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
